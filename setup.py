0fcba7154e084a3ec293161de5cedba0fe51bf0c
muellerzr <muellerzr@gmail.com>
2022-11-15 23:14:44 -0500

    UPdate dep

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setuptools.setup(
     url = cfg['git_url'],
     packages = setuptools.find_packages(),
     include_package_data = True,
-    install_requires = requirements,
+    install_requires = "nbdev_extensions @ git+https://github.com/muellerzr/nbdev-extensions#egg=nbdev-extensions",
     extras_require={ 'dev': dev_requirements },
     dependency_links = cfg.get('dep_links','').split(),
     python_requires  = '>=' + cfg['min_python'],
